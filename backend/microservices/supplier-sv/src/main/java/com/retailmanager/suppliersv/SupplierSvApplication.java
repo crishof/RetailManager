f79d86e53f7bf5eaf1de60eebe6bf4d97c667157
Cristian Hoffmann <cristianhof@gmail.com>
2025-12-10 16:31:48 +0100

    add supplier sv

---
+++ backend/microservices/supplier-sv/src/main/java/com/retailmanager/suppliersv/SupplierSvApplication.java
@@ -0,0 +1,13 @@
+package com.retailmanager.suppliersv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierSvApplication.class, args);
+    }
+
+}
---
+++ backend/microservices/supplier-sv/src/test/java/com/retailmanager/suppliersv/SupplierSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailmanager.suppliersv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
