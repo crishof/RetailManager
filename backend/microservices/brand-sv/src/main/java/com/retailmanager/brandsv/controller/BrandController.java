a5b57dc186dfb4c42fc8ee7e3a8f75623c5a25d3
Cristian Hoffmann <cristianhof@gmail.com>
2025-12-12 19:09:17 +0100

    first service connection

--- backend/microservices/brand-sv/src/main/java/com/retailmanager/brandsv/controller/BrandController.java
+++ backend/microservices/brand-sv/src/main/java/com/retailmanager/brandsv/controller/BrandController.java
@@ -1,5 +1,6 @@
 package com.retailmanager.brandsv.controller;
 
+import com.retailmanager.brandsv.service.CategoryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/brands")
 public class BrandController {
 
+    private final CategoryClient categoryClient;
+
+    public BrandController(CategoryClient categoryClient) {
+        this.categoryClient = categoryClient;
+    }
+
     @GetMapping("/status")
     public String getStatus() {
-        return "Brand Service is up and running!";
+
+        String categoryStatus = categoryClient.getCategoryStatus();
+        return "Brand Service is up and running! Category Service Status: " + categoryStatus;
     }
 }
---
+++ backend/microservices/brand-sv/src/main/java/com/retailmanager/brandsv/service/CategoryClient.java
@@ -0,0 +1,22 @@
+package com.retailmanager.brandsv.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class CategoryClient {
+
+    private final WebClient webClient;
+
+    public CategoryClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public String getCategoryStatus() {
+        return webClient.get()
+                .uri("http://category-sv:9002/api/categories/status")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+}
