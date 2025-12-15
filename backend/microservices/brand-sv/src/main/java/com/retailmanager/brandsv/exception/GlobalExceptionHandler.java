9f48fbb69b24738d5829dfb1d4aa7f5019f41f82
Cristian Hoffmann <cristianhof@gmail.com>
2025-12-15 16:07:50 +0100

    add exception handler

---
+++ backend/microservices/brand-sv/src/main/java/com/retailmanager/brandsv/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.retailmanager.brandsv.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleBadRequest(InvalidRequestException ex,
+                                     HttpServletRequest request) {
+        return new ApiError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(), "Bad Request",
+                ex.getMessage(),
+                request.getRequestURI());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNotFound(ResourceNotFoundException ex,
+                                   HttpServletRequest request) {
+        return new ApiError(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(), "Not Found",
+                ex.getMessage(),
+                request.getRequestURI());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleGeneric(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        return new ApiError(
+                Instant.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error",
+                ex.getMessage(),
+                request.getRequestURI());
+    }
+}
