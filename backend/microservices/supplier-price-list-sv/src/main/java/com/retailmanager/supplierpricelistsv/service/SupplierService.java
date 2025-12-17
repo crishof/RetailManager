2d4ba1f2c95e3a1db5143ee2d2633b7773a2c67a
Cristian Hoffmann <cristianhof@gmail.com>
2025-12-17 23:05:45 +0100

    supplier service

---
+++ backend/microservices/supplier-price-list-sv/src/main/java/com/retailmanager/supplierpricelistsv/service/SupplierService.java
@@ -0,0 +1,11 @@
+package com.retailmanager.supplierpricelistsv.service;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SupplierService {
+
+    List<String> getBrandsBySupplier(UUID supplierId);
+
+    List<String> getAllBrands();
+}
