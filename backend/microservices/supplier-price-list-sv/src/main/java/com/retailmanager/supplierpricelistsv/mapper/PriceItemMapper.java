6987345d31a99f2cee0aa20e824c76b50c0a9695
Cristian Hoffmann <cristianhof@gmail.com>
2025-12-19 17:47:52 +0100

    mapper

---
+++ backend/microservices/supplier-price-list-sv/src/main/java/com/retailmanager/supplierpricelistsv/mapper/PriceItemMapper.java
@@ -0,0 +1,16 @@
+package com.retailmanager.supplierpricelistsv.mapper;
+
+import com.retailmanager.supplierpricelistsv.dto.PriceItemResponse;
+import com.retailmanager.supplierpricelistsv.model.SupplierPriceItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface PriceItemMapper {
+
+    PriceItemResponse toDto(SupplierPriceItem supplierPriceItem);
+    
+}
