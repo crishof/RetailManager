c3092876b9f289f90161ec78fd6bacee51f2404e
Cristian Hoffmann <cristianhof@gmail.com>
2025-12-22 19:07:50 +0100

    model

--- backend/microservices/supplier-sv/src/main/java/com/retailmanager/suppliersv/dto/SupplierRequest.java
+++ backend/microservices/supplier-sv/src/main/java/com/retailmanager/suppliersv/dto/SupplierRequest.java
@@ -1,4 +1,16 @@
 package com.retailmanager.suppliersv.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class SupplierRequest {
+
+    private String name;
+    private String taxId;
+    private String legalName;
+    private UUID addressId;
 }
