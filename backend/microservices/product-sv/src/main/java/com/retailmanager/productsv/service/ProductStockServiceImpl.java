f33fb2fdff134a3f06ebfca3a5c1825cca14444d
Cristian Hoffmann <cristianhof@gmail.com>
2026-01-02 15:46:49 +0100

    import service

--- backend/microservices/product-sv/src/main/java/com/retailmanager/productsv/service/ProductStockServiceImpl.java
+++ backend/microservices/product-sv/src/main/java/com/retailmanager/productsv/service/ProductStockServiceImpl.java
@@ -1,7 +1,18 @@
 package com.retailmanager.productsv.service;
 
+import com.retailmanager.productsv.dto.InvoiceUpdateRequest;
+import com.retailmanager.productsv.dto.OrderUpdateRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductStockServiceImpl implements ProductStockService {
+    @Override
+    public void updateFromInvoice(InvoiceUpdateRequest request) {
+        //TODO implement method
+    }
+
+    @Override
+    public void updateStockFromOrder(OrderUpdateRequest request) {
+        //TODO implement method
+    }
 }
