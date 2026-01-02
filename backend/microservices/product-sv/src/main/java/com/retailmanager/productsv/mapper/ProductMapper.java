b200170b0e416b6ad8c502b040a88d3471bc9929
Cristian Hoffmann <cristianhof@gmail.com>
2026-01-02 12:52:30 +0100

    mapper

--- backend/microservices/product-sv/src/main/java/com/retailmanager/productsv/mapper/ProductMapper.java
+++ backend/microservices/product-sv/src/main/java/com/retailmanager/productsv/mapper/ProductMapper.java
@@ -1,4 +1,28 @@
 package com.retailmanager.productsv.mapper;
 
+import com.retailmanager.productsv.dto.ProductRequest;
+import com.retailmanager.productsv.dto.ProductResponse;
+import com.retailmanager.productsv.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface ProductMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Product toEntity(ProductRequest request);
+
+    ProductResponse toResponse(Product entity);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromRequest(
+            ProductRequest request,
+            @MappingTarget Product entity);
 }
