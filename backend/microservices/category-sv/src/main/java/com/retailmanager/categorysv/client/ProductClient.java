a4cc897c4ca8c1291b632256747b2f49485bfab9
Cristian Hoffmann <cristianhof@gmail.com>
2026-01-08 16:32:07 +0100

    add product client

---
+++ backend/microservices/category-sv/src/main/java/com/retailmanager/categorysv/client/ProductClient.java
@@ -0,0 +1,25 @@
+package com.retailmanager.categorysv.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class ProductClient {
+
+    private static final String BASE_URL = "http://product-sv:8080/api/v1/products";
+    private final WebClient webClient;
+
+    public ProductClient(WebClient.Builder builder) {
+        this.webClient = builder
+                .baseUrl(BASE_URL)
+                .build();
+    }
+
+    public void deleteProductsCategory(UUID categoryId) {
+        //TODO implement method
+    }
+}
