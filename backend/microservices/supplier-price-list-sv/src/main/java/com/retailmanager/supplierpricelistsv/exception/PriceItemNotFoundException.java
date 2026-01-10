70654746e115c1ba51aa2339925e677c6de6b277
Cristian Hoffmann <cristianhof@gmail.com>
2026-01-10 19:57:44 +0100

    price list exception

--- backend/microservices/supplier-price-list-sv/src/main/java/com/retailmanager/supplierpricelistsv/exception/PriceItemNotFoundException.java
+++ backend/microservices/supplier-price-list-sv/src/main/java/com/retailmanager/supplierpricelistsv/exception/PriceItemNotFoundException.java
@@ -3,7 +3,6 @@ package com.retailmanager.supplierpricelistsv.exception;
 import java.util.UUID;
 
 public class PriceItemNotFoundException extends RuntimeException {
-
     public PriceItemNotFoundException(UUID id) {
         super("Product with id " + id + " not found");
     }
