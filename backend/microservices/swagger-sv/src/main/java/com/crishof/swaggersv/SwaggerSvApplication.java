00c1323a01af6aa5cc74624c7ca15a20736ffc1f
Cristian Hoffmann <cristianhof@gmail.com>
2026-01-15 12:31:29 +0100

    add swagger service

---
+++ backend/microservices/swagger-sv/src/main/java/com/crishof/swaggersv/SwaggerSvApplication.java
@@ -0,0 +1,13 @@
+package com.crishof.swaggersv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerSvApplication.class, args);
+    }
+
+}
---
+++ backend/microservices/swagger-sv/src/test/java/com/crishof/swaggersv/SwaggerSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crishof.swaggersv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
